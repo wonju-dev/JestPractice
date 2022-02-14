96cd23b77195fd5f84c5627807ba9aae6a4e214d
wonju-dev <wonjulee.dev@gmail.com>
2022-02-15 02:06:35 +0900

    string match

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -1,8 +1,7 @@
-test("first test", () => {
-  expect(null).toBeNull();
-  expect(NaN).toBeNaN();
-  expect(1).toBeDefined();
-  expect(undefined).toBeUndefined();
-  expect(true).toBeTruthy();
-  expect(false).toBeFalsy();
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/); // I가 포함되지 않음
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/); // stop이 포함되지 않음
 });
